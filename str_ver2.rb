fb937eff576d1483c805d7d0705bb6eda0d683dc
aikanakamura <aika@occ.co.jp>
2014-03-26 23:34:37 +0900

    例題の追加

---
+++ str_ver2.rb
@@ -0,0 +1,7 @@
+greeting = "Hello,Ruby!"
+# 変数にhello,rubyを入れる
+print("greeting Hello, Ruby!")
+# ""で変数名を囲うとそのまま変数名が文字列として表示される
+print("#{greenting} Hello,Ruby")
+# #{}で囲むと変数が呼び出されて、Hello,Ruby!が2回表示される
+
