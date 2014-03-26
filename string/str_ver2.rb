f3c4f6c58f43f0da74b4f17435513187ed940854
aikanakamura <aika@occ.co.jp>
2014-03-26 22:45:44 +0900

    2つ目の例題のテキストです。変数に文字列入れて、表示する説明

---
+++ string/str_ver2.rb
@@ -0,0 +1,6 @@
+greeting = "Hello, Ruby!"
+# 変数greentingの中に"Hello, Ruby!"を入れる
+print("greenting Hello, Ruby!")
+# 変数名を""で囲うとそのまま表示される
+print("#{greenting} Hello, Ruby!")
+# 変数を呼び出して"Hello,Ruby"が2回でるよ
