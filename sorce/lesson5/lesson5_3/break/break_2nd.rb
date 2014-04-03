b4cae7cbc078624ecda48b2e84605829c9dd5ef0
aikanakamura <tea.jack.cat15@gmail.com>
2014-04-03 11:41:06 +0900

    ループが入れ子のパターンの修正

--- sorce/lesson5/lesson5_3/break/break_2nd.rb
+++ sorce/lesson5/lesson5_3/break/break_2nd.rb
@@ -1,13 +1,12 @@
 [1, 2, 3, 4].each do |i|
-  puts("iの中身#{i}")
-count = 1
-loop {
-  if count == 3
-    break
+puts("iの中身#{i}")
+  count = 1
+  loop {
+     if count == 3
+     break
   end
   puts ("loop中#{count}回目")
   count += 1
-}
-#  puts("iの中身#{i}")
+  }
   break
 end
